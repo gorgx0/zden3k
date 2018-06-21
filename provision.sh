45befc7a4301c5f4809ce54bc9e16f1b3fdc87d1
Marcin <marcin.krasowski@gmail.com>
2018-06-22 00:23:53 +0200

    changed apt-get line

--- provision.sh
+++ provision.sh
@@ -4,7 +4,22 @@ locale-gen en_UK.UTF-8
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 apt-get update
-apt-get install -y tmux mc vim  bash-completion man locate unzip git tree
-apt-get install -y libapache2-mod-php7.0 mariadb-server mariadb-client locate mlocate php-fpm
-apt-get install -y composer php7.0-xml php7.0-sqlite3
-# php-pear
+apt-get install -y \
+  tmux \
+  mc \
+  vim \
+  bash-completion \
+  man \
+  locate \
+  unzip \
+  git \
+  tree \
+  libapache2-mod-php7.0 \
+  mariadb-server \
+  mariadb-client \
+  locate \
+  mlocate \
+  php-fpm \
+  composer \
+  php7.0-xml \
+  php7.0-sqlite3
