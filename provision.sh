1af82be97151cbe765a5a5fce5c9ea8cc9348047
Marcin <marcin.krasowski@gmail.com>
2018-06-21 23:13:46 +0200

    php-xml added

--- provision.sh
+++ provision.sh
@@ -6,5 +6,5 @@ dpkg-reconfigure locales
 apt-get update
 apt-get install -y tmux mc vim  bash-completion man locate unzip git
 apt-get install -y libapache2-mod-php7.0 mariadb-server mariadb-client locate mlocate php-fpm
-apt-get install -y composer 
+apt-get install -y composer php7.0-xml
 # php-pear
