b14d27ec92fa4457f4b1c834c0a0cf041daf76f6
Marcin <marcin.krasowski@gmail.com>
2018-06-21 17:10:13 +0200

    initial commit

---
+++ provision.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+locale-gen en_UK.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+apt-get update
+apt-get install -y tmux mc vim  bash-completion man locate unzip git
+apt-get install -y libapache2-mod-php7.0 mariadb-server mariadb-client locate mlocate php-fpm
+# php-pear
